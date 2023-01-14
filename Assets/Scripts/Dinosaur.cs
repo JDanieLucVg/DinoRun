3660bddc78760d8030db2738b7b0edaa4490ef34
Jesus Daniel Lucas Villegas <lucas@MBP-de-Lucas.hitronhub.home>
2023-01-14 14:36:41 -0600

    Creacion de juego

---
+++ Assets/Scripts/Dinosaur.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : MonoBehaviour
+{
+    //Fuerza del salto
+    [SerializeField] private float upForce;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float radius;
+
+
+    private Rigidbody2D dinoRb;
+    private Animator dinoAnimator;
+
+    void Start()
+    {
+        dinoRb = GetComponent<Rigidbody2D>();
+        dinoAnimator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
+        dinoAnimator.SetBool("IsGraunded", isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                dinoRb.AddForce(Vector2.up * upForce);
+
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, radius);   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameManager.Instance.ShowGameOverScreen();
+            dinoAnimator.SetTrigger("Die");
+            Time.timeScale = 0f;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * GameManager.Instance.GetscrolISpeed() * Time.deltaTime);
+
+    }
+}
